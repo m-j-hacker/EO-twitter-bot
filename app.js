6d735d4560f9ba2577166aa70bbd22f64c603dd8
Michael Hacker <mjhacker@gmail.com>
2020-07-30 15:14:43 -0500

    Make module exports for config.js

---
+++ app.js
@@ -0,0 +1,3 @@
+// app.js
+// This file houses the main section of code for the Twitter bot
+// Ĉi tio dosiero enhavas la ĉefan parton de kodo por la roboto de Twitter
---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+// This file will store our configuration options
+// Ĉi tiu dosiero konservos niajn agordojn
+
+module.exports = {
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
+}
