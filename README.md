8ea783d1d06f918e0f5f6765084b703e26aa1ad4
Michael Hacker <mjhacker@gmail.com>
2020-08-25 15:00:06 -0500

    Fill out main appJS body, update readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # EO-twitter-bot
 Ĉi tio estas projekto por krei reta roboto kiu interagas kun Twitter (plibonigi, sekvi, ktp)
+
+La inspiro por ĉi tiu projekto venis el https://codeburst.io/build-a-simple-twitter-bot-with-node-js-in-just-38-lines-of-code-ed92db9eb078
+Bonvolu, subteni @BrandonMorelli!
--- app.js
+++ app.js
@@ -1,3 +1,58 @@
 // app.js
 // This file houses the main section of code for the Twitter bot
-// Ĉi tio dosiero enhavas la ĉefan parton de kodo por la roboto de Twitter
+// Ĉi tiu dosiero enhavas la ĉefan parton de kodo por la roboto de Twitter
+
+const Twitter = require('twitter');
+const config = require('./config.js');
+
+// Using our config options, we will start the bot
+// Uzante niajn agordojn, ni komencos la roboton
+
+const T = new Twitter(config);
+
+// Search Parameters
+// Serĉaj Parametroj
+// Atentu: Twitter ne subtenas EO per sia propra kodo.
+// Se tio ŝanĝos, tiam mi ĝisdatigos ĉi tiun programeron. - @m-j-hacker
+let params = {
+    q: '#esperanto',
+    count: 10,
+    result_type: 'recent',
+    // lang: 'eo'
+}
+
+// GET request for posts
+// GET peto por afiŝoj
+
+T.get('search/tweets', params, function(err, data, response) {
+    if (!err) {
+        // Se eraroj ne ekzistas, komencu kodon
+
+        // Loop through returned tweets
+        // Interaciu tra redonitaj afiŝoj
+        for(let i = 0; i < data.statuses.length; i++) {
+            // Get Tweet ID from returned data
+            // Prenu Tweet ID de redonita datumo
+            let id = { id: data.statuses[i].id_str }
+            // Try to favorite the selected message
+            // Provu plej ŝati la elektitan afiŝon
+            T.post('favorites/create', id, function(err, response) {
+                // If we are not successful, log the URL of the tweet
+                // Se ni ne sukcesas, logu la adreson de la afiŝo
+                if(err) {
+                    console.log(err[0].message);
+                }
+                // If we are succesful, log the URL of the tweet
+                // Se ni sukcesas, logu la adreson de la afiŝo
+                else {
+                    let username = response.user.screen_name;
+                    let tweetId = response.id_str;
+                    console.log('Plej ŝatis: ', `https://twitter.com/${username}/status/${tweetId}`)
+                }
+            });
+        }
+    } else {
+        // Se eraro ekzistas, redonu logon
+        console.log(err)
+    }
+})
